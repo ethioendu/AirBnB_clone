ec7c06385635d8bdf1919fbfd6471594dcafeedd
Wasealex <wasealex27@gmail.com>
2024-02-08 14:06:40 +0300

    notworking

--- models/base_model.py
+++ models/base_model.py
@@ -9,12 +9,20 @@ import uuid
 class BaseModel:
     """
     """
-    def __init__(self):
+    def __init__(self,  *args, **kwargs):
         """
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.update_at = self.created_at
+        if kwargs:
+            time = "%Y-%m-%dT%H:%M:%S.%f"
+            kwargs.pop("__class__", None)
+            for key, value in kwargs.items():
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(value, time)
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
     def __str__(self):
         """
