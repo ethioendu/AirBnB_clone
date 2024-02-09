015672cf1c07517bdf52a905235162a21940f343
Wasealex <wasealex27@gmail.com>
2024-02-09 08:39:25 +0300

    storage from BaseModel

--- models/base_model.py
+++ models/base_model.py
@@ -5,6 +5,7 @@
 
 from datetime import datetime
 import uuid
+from models.__init__ import storage
 
 time = "%Y-%m-%dT%H:%M:%S.%f"
 class BaseModel:
@@ -23,6 +24,8 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
+            storage.new(self)
+            storage.save()
 
     def __str__(self):
         """
@@ -35,6 +38,7 @@ class BaseModel:
         """
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
---
+++ models/engine/file_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+"""
+import json
+
+
+class FileStorage:
+    """
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """
+        """
+        return self.__objects
+
+    def new(self, obj):
+        """
+        """
+        if obj is not None:
+            key = "{}.{}".format(type(obj).__name__, obj.id)
+            self.__objects[key] = obj
+
+    def save(self):
+        obj_dict = {}
+        for key, obj in self.__objects.items():
+            obj_dict[key] = obj.to_dict()
+
+        with open(self.__file_path, "w") as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """
+        """
+        try:
+            with open(self.__file_path, "r") as f:
+                obj_dict = json.load(f)
+
+            from models.base_model import BaseModel
+            for key, value in obj_dict.items():
+                class_name, obj_id = key.split(".")
+                model_class = eval(class_name)
+                obj = model_class(**value)
+                self.new(obj)
+        except:
+            pass
