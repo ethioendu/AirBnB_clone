e627cf8830a3b308c096c5c33567eb0ea772c886
Wasealex <wasealex27@gmail.com>
2024-02-11 10:56:35 +0300

    complete test for BaseModel

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -15,5 +15,18 @@ class TestBaseModel(unittest.TestCase):
         self.assertIsInstance(base_model.created_at, datetime.datetime)
         self.assertIsInstance(base_model.updated_at, datetime.datetime)
 
+    def test_save(self):
+        base_model = BaseModel()
+        old_updated_at = base_model.updated_at
+        base_model.save()
+        self.assertNotEqual(old_updated_at, base_model.updated_at)
+
+    def test_to_dict(self):
+        base_model = BaseModel()
+        base_model_dict = base_model.to_dict()
+        self.assertIsInstance(base_model_dict, dict)
+        self.assertIn("__class__", base_model_dict)
+        self.assertEqual(base_model_dict["__class__"], "BaseModel")
+
 if __name__ == "__main__":
     unittest.main()
