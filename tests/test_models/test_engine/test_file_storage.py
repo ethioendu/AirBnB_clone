990d8ab0de53550b857647221987d52757b45c89
Wasealex <wasealex27@gmail.com>
2024-02-11 16:20:27 +0300

    unitest file

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -12,7 +12,6 @@ class FileStorageTestCase(unittest.TestCase):
         self.storage = FileStorage()
         self.storage._FileStorage__file_path = self.file_path
         self.base_model = BaseModel()
-        self.base_model.save()
 
     def tearDown(self):
         if os.path.exists(self.file_path):
@@ -36,6 +35,41 @@ class FileStorageTestCase(unittest.TestCase):
         new_storage.reload()
         all_objects = new_storage.all()
         self.assertIn('BaseModel.{}'.format(self.base_model.id), all_objects)
+
+    def test_save_existing_object(self):
+        self.base_model.name = "Test Model"
+        self.storage.save()
+
+        new_storage = FileStorage()
+        new_storage._FileStorage__file_path = self.file_path
+        new_storage.reload()
+
+        loaded_model = new_storage.all()['BaseModel.{}'.format(self.base_model.id)]
+        self.assertEqual(loaded_model.name, "Test Model")
+
+    def test_save_multiple_objects(self):
+        base_model1 = BaseModel()
+        base_model2 = BaseModel()
+        base_model3 = BaseModel()
+        base_model1.name = "Model 1"
+        base_model2.name = "Model 2"
+        base_model3.name = "Model 3"
+        self.storage.save()
+
+        new_storage = FileStorage()
+        new_storage._FileStorage__file_path = self.file_path
+        new_storage.reload()
+
+        all_objects = new_storage.all()
+        self.assertIn('BaseModel.{}'.format(base_model1.id), all_objects)
+        self.assertIn('BaseModel.{}'.format(base_model2.id), all_objects)
+        self.assertIn('BaseModel.{}'.format(base_model3.id), all_objects)
+        self.assertEqual(all_objects['BaseModel.{}'.format(base_model1.id)].name
+                         , "Model 1")
+        self.assertEqual(all_objects['BaseModel.{}'.format(base_model2.id)].name
+                         , "Model 2")
+        self.assertEqual(all_objects['BaseModel.{}'.format(base_model3.id)].name
+                         , "Model 3")
 
 if __name__ == '__main__':
     unittest.main()
